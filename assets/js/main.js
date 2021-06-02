e81f3608e49616c331c976e95acf68e3eb5dc8cb
Chris McKelt <chris@mckelt.com>
2021-06-03 07:21:16 +0800

    SAVEPOINT

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,16 @@
 (function () {
   'use strict';
 
-  const stravaCardBody = document.querySelector('.strava-card .card-body');
-  let timeoutId;
-
-  if (stravaCardBody) {
-   
-  }
 
   if (isDateTimeFormatSupported()) {
     formatDates();
   }
 
   ////////////////
-
   function formatDates() {
     const language = navigator.language;
-
     const dateTimeOptions = { month: 'long', year: 'numeric', day: 'numeric' };
-    const dateTimeFormat = new Intl.DateTimeFormat(language, dateTimeOptions);
+    const dateTimeFormat = new Intl.DateTimeFormat(language);
 
     const relativeTimeOptions = { numeric: 'auto' };
     // @ts-ignore
@@ -52,41 +44,5 @@
     return typeof Intl.RelativeTimeFormat === 'function';
   }
 
-  /**
-   * @param {number} distance
-   */
-  function toKm(distance) {
-    return (distance / 1000).toFixed(2);
-  }
-
-  /**
-   * @param {number} totalTimeInSeconds
-   */
-  function formatTime(totalTimeInSeconds) {
-    const oneHourInSeconds = 3600;
-    let hours = Math.floor(totalTimeInSeconds / oneHourInSeconds);
-    let minutes = Math.floor((totalTimeInSeconds - (hours * oneHourInSeconds)) / 60);
-    let seconds = totalTimeInSeconds - (hours * oneHourInSeconds) - (minutes * 60);
-
-    if (minutes < 10) {
-      // @ts-ignore
-      minutes = `0${minutes}`;
-    }
-    
-    if (seconds < 10) {
-      // @ts-ignore
-      seconds = `0${seconds}`;
-    }
-
-    if (hours > 0) {
-      return `${hours}:${minutes}:${seconds}`;
-    }
-
-    return `${minutes}:${seconds}`;
-  }
       
-
-  function showStartFetchingMessage() {
-    stravaCardBody.innerHTML = '<p><small>🏃 Remembering Thoughts...</small></p>';
-  }
 }());
